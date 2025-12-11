2fd534fed168660b9776cd47ae36d9b17bc1d087
Maxim Spirin <n6541270@gmail.com>
2025-12-11 15:46:52 +0900

    Добавили файл masks.py, содержащий функционал маскировки карты

---
+++ src/masks.py
@@ -0,0 +1,27 @@
+def get_mask_card_number(number_card: str) -> str:
+    """Функция принимает на вход номер карты и возвращает ее маску, в формате XXXX XX** **** XXXX"""
+    digits_card_number = []
+    number_counter = 0
+
+    for i in range(len(number_card)):
+        # Проверяем если индекс чисел от 6-11, тогда делаем замену на *
+        if 6 <= i <= 11:
+            number_counter += 1
+            digits_card_number.append("*")
+        else:
+            number_counter += 1
+            digits_card_number.append(number_card[i])
+
+        if number_counter == 4:
+            number_counter = 0
+            digits_card_number.append(" ")
+
+    formatted_card_number = "".join(digits_card_number).strip()
+    return formatted_card_number
+
+
+def get_mask_account(number_account: str) -> str:
+    """Функция принимает на вход номер счета и возвращает его маску, в формате **XXXX"""
+    digits_account_number = [num for num in number_account[-4:]]
+    mask_account_number = "**" + "".join(digits_account_number)
+    return mask_account_number
