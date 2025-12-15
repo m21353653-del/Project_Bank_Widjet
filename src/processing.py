c952349f0a4f2dc170043d1f0bcfe65e07cc8f6e
m21353653-del <m21353653@gmail.com>
2025-12-15 15:50:37 +0900

    add sort_by_date function in processing.py

--- src/processing.py
+++ src/processing.py
@@ -9,3 +9,11 @@ def filter_by_state(my_list: list, state: str = "EXECUTED") -> list:
             new_list.append(item)
 
     return new_list
+
+
+def sort_by_date(my_list: list, is_reverse: bool = True) -> list:
+    ''' Функция принимает список словарей
+     и необязательный параметр - метод сортировки
+     Возвращает новый список словарей сортированый по дате'''
+    list_date_sorted = sorted(my_list, key=lambda date: date["date"], reverse=is_reverse)
+    return list_date_sorted
