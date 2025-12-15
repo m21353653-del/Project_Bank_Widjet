55994611454106e3b5b64896f313a46ca91bd312
m21353653-del <m21353653@gmail.com>
2025-12-15 16:32:54 +0900

    add README.md and fixes

--- src/processing.py
+++ src/processing.py
@@ -1,19 +1,19 @@
 def filter_by_state(my_list: list, state: str = "EXECUTED") -> list:
-    ''' Функцция возвращает список словарей,
+    """Функцция возвращает список словарей,
     если значение state в словаре = "EXECUTED"
-    Возвращает new_list с значение state в словаре = "EXECUTED" '''
-    new_list = []
+    Возвращает new_list с значение state в словаре = "EXECUTED" """
+    new_list_formated = []
 
     for item in my_list:
         if item["state"] == state:
-            new_list.append(item)
+            new_list_formated.append(item)
 
-    return new_list
+    return new_list_formated
 
 
 def sort_by_date(my_list: list, is_reverse: bool = True) -> list:
-    ''' Функция принимает список словарей
-     и необязательный параметр - метод сортировки
-     Возвращает новый список словарей сортированый по дате'''
+    """Функция принимает список словарей
+    и необязательный параметр - метод сортировки
+    Возвращает новый список словарей сортированый по дате"""
     list_date_sorted = sorted(my_list, key=lambda date: date["date"], reverse=is_reverse)
     return list_date_sorted
