4af6eebb03a064b8ec4c0eb37be650f11f5b894c
m21353653-del <m21353653@gmail.com>
2025-12-18 17:38:19 +0900

    add test_masks

--- src/masks.py
+++ src/masks.py
@@ -1,7 +1,14 @@
+from enum import EnumMeta
+
+
 def get_mask_card_number(number_card: str) -> str:
     """Функция принимает на вход номер карты и возвращает ее маску, в формате XXXX XX** **** XXXX"""
     digits_card_number = []
     number_counter = 0
+
+    # Длина номера карты должна быть == 16
+    if len(number_card) < 16 or len(number_card) > 16:
+        return "Номер карты должен быть равен 16!"
 
     for i in range(len(number_card)):
         # Проверяем если индекс чисел от 6-11, тогда делаем замену на *
