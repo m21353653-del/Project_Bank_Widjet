6cd6e1261e8b9d6e9805a7f45456350dd22a19d2
m21353653-del <m21353653@gmail.com>
2025-12-21 21:22:56 +0900

    add info in README.md

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -2,27 +2,26 @@ import pytest
 from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
 
 
-def test_filter_by_currency(my_list):
+@pytest.mark.parametrize(
+    "result",
+    [
+        (
+            {
+                "id": 939719570,
+                "state": "EXECUTED",
+                "date": "2018-06-30T02:08:58.425572",
+                "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+                "description": "Перевод организации",
+                "from": "Счет 75106830613657916952",
+                "to": "Счет 11776614605963066702",
+            }
+        ),
+    ],
+)
+def test_filter_by_currency(my_list, result):
     generation = filter_by_currency(my_list, "USD")
 
-    assert next(generation) == {
-        "id": 939719570,
-        "state": "EXECUTED",
-        "date": "2018-06-30T02:08:58.425572",
-        "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
-        "description": "Перевод организации",
-        "from": "Счет 75106830613657916952",
-        "to": "Счет 11776614605963066702",
-    }
-    assert next(generation) == {
-        "id": 142264268,
-        "state": "EXECUTED",
-        "date": "2019-04-04T23:20:05.206878",
-        "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
-        "description": "Перевод со счета на счет",
-        "from": "Счет 19708645243227258542",
-        "to": "Счет 75651667383060284188",
-    }
+    assert next(generation) == result
 
 
 def test_filter_no_transactions(my_list):
