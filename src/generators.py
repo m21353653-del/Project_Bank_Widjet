36f568a3d6700c299b2b840fdd0cfc78da23c927
m21353653-del <m21353653@gmail.com>
2025-12-21 20:59:29 +0900

    fixes

--- src/generators.py
+++ src/generators.py
@@ -1,10 +1,9 @@
-from hmac import trans_36
 from typing import Iterable
 
 
 def filter_by_currency(list_dict: list, val: str) -> Iterable[dict]:
-    ''' Принимает список словарей с транзакциями и возвращает итератор, который поочередно выдает транзакции,
-    где валюта операции соответствует заданной '''
+    """Принимает список словарей с транзакциями и возвращает итератор, который поочередно выдает транзакции,
+    где валюта операции соответствует заданной"""
 
     for trans in list_dict:
         if trans["operationAmount"]["currency"]["code"] == val:
@@ -12,13 +11,14 @@ def filter_by_currency(list_dict: list, val: str) -> Iterable[dict]:
 
 
 def transaction_descriptions(list_dict: list, start=0) -> str:
-    ''' принимает список словарей с транзакциями и возвращает описание каждой операции по очереди '''
+    """принимает список словарей с транзакциями и возвращает описание каждой операции по очереди"""
     while True:
         yield list_dict[start]["description"]
         start += 1
 
+
 def card_number_generator(start: int, stop: int) -> str:
-    ''' Генератор принимает начальное и конечное значения для генерации диапазона номеров. '''
+    """Генератор принимает начальное и конечное значения для генерации диапазона номеров."""
     for num in range(start, stop + 1):
         number_card = f"{num:016d}"
         formatted = f"{number_card[:4]} {number_card[4:8]} {number_card[8:12]} {number_card[12:16]}"
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -6,35 +6,23 @@ def test_filter_by_currency(my_list):
     generation = filter_by_currency(my_list, "USD")
 
     assert next(generation) == {
-          "id": 939719570,
-          "state": "EXECUTED",
-          "date": "2018-06-30T02:08:58.425572",
-          "operationAmount": {
-              "amount": "9824.07",
-              "currency": {
-                  "name": "USD",
-                  "code": "USD"
-              }
-          },
-          "description": "Перевод организации",
-          "from": "Счет 75106830613657916952",
-          "to": "Счет 11776614605963066702"
-      }
+        "id": 939719570,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+        "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод организации",
+        "from": "Счет 75106830613657916952",
+        "to": "Счет 11776614605963066702",
+    }
     assert next(generation) == {
-              "id": 142264268,
-              "state": "EXECUTED",
-              "date": "2019-04-04T23:20:05.206878",
-              "operationAmount": {
-                  "amount": "79114.93",
-                  "currency": {
-                      "name": "USD",
-                      "code": "USD"
-                  }
-              },
-              "description": "Перевод со счета на счет",
-              "from": "Счет 19708645243227258542",
-              "to": "Счет 75651667383060284188"
-       }
+        "id": 142264268,
+        "state": "EXECUTED",
+        "date": "2019-04-04T23:20:05.206878",
+        "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод со счета на счет",
+        "from": "Счет 19708645243227258542",
+        "to": "Счет 75651667383060284188",
+    }
 
 
 def test_filter_no_transactions(my_list):
@@ -63,14 +51,14 @@ def test_transaction_no_enmpty():
 # card_number_generator
 def test_card_number_generator():
     generation = card_number_generator(1, 5)
-    assert next(generation) == '0000 0000 0000 0001'
-    assert next(generation) == '0000 0000 0000 0002'
-    assert next(generation) == '0000 0000 0000 0003'
-    assert next(generation) == '0000 0000 0000 0004'
-    assert next(generation) == '0000 0000 0000 0005'
+    assert next(generation) == "0000 0000 0000 0001"
+    assert next(generation) == "0000 0000 0000 0002"
+    assert next(generation) == "0000 0000 0000 0003"
+    assert next(generation) == "0000 0000 0000 0004"
+    assert next(generation) == "0000 0000 0000 0005"
 
 
 def test_card_number_generator_2():
     generation = card_number_generator(9999999999999998, 9999999999999999)
-    assert next(generation) == '9999 9999 9999 9998'
-    assert next(generation) == '9999 9999 9999 9999'
+    assert next(generation) == "9999 9999 9999 9998"
+    assert next(generation) == "9999 9999 9999 9999"
