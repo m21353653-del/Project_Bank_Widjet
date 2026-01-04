b30a51280da8944d6c727aa53d7c8c387441face
m21353653-del <m21353653@gmail.com>
2026-01-04 20:15:43 +0900

    add get_transaction_amount and fixes

--- src/utils.py
+++ src/utils.py
@@ -17,23 +17,3 @@ def get_transaction_data(json_file: str) -> Any:
         return list_operations
     except TypeError:
         return list_operations
-
-
-def get_transaction_amount(transactions: list[dict]) -> float:
-    ''' Функция принимает на вход транзакцию и возвращает сумму '''
-    amount = 0
-
-    for trans in transactions:
-        try:
-            if trans["operationAmount"]["currency"]["code"] == "RUB":
-                amount += float(trans["operationAmount"]["amount"])
-            else:
-                pass
-        except KeyError:
-            continue
-
-    return amount
-
-
-result = get_transaction_data("../data/operations.json")
-print(get_transaction_amount(result))
