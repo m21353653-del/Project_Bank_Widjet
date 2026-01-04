eeae2d2c26154c1bd434fe42bb85eace51fdbf1a
m21353653-del <m21353653@gmail.com>
2026-01-04 18:59:45 +0900

    add utils.py

---
+++ src/utils.py
@@ -0,0 +1,22 @@
+import json
+from typing import Any
+
+
+def get_transaction_data(json_file: str) -> Any:
+    """Функция принимает json файл и возвращает список словарей с даными об операциях"""
+    list_operations = []
+
+    try:
+        with open(json_file, "r", encoding="utf-8") as file:
+            list_operations = json.load(file)
+
+        return list_operations
+    except FileNotFoundError:
+        return list_operations
+    except json.JSONDecodeError:
+        return list_operations
+    except TypeError:
+        return list_operations
+
+
+print(get_transaction_data("../data/operations.json"))
