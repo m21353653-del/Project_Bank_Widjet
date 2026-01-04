0d6cfe33a563254b7d69071c4382f886e6ae1b96
m21353653-del <m21353653@gmail.com>
2026-01-04 19:32:01 +0900

    add .env and add external_api.py

--- src/utils.py
+++ src/utils.py
@@ -19,4 +19,21 @@ def get_transaction_data(json_file: str) -> Any:
         return list_operations
 
 
-print(get_transaction_data("../data/operations.json"))
+def get_transaction_amount(transactions: list[dict]) -> float:
+    ''' Функция принимает на вход транзакцию и возвращает сумму '''
+    amount = 0
+
+    for trans in transactions:
+        try:
+            if trans["operationAmount"]["currency"]["code"] == "RUB":
+                amount += float(trans["operationAmount"]["amount"])
+            else:
+                pass
+        except KeyError:
+            continue
+
+    return amount
+
+
+result = get_transaction_data("../data/operations.json")
+print(get_transaction_amount(result))
