40bba47de3d7a9017fd8568a07aa2685feda2b96
m21353653-del <m21353653@gmail.com>
2026-01-05 13:39:31 +0900

    fixes masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,9 +1,9 @@
 import logging
 
 logger = logging.getLogger(__name__)
-file_handler = logging.FileHandler("../logs/masks.log", 'w', encoding="utf-8")
+file_handler = logging.FileHandler("../logs/masks.log", "w", encoding="utf-8")
 stream_handler = logging.StreamHandler()
-file_formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s')
+file_formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s %(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
 logger.setLevel(logging.DEBUG)
