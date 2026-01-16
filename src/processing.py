66ba22f0c714c93a70aa106cb15a49853d0fa4fe
m21353653-del <m21353653@gmail.com>
2026-01-16 11:59:35 +0900

    add func process_bank_search in src/processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,6 @@
+import re
+
+
 def filter_by_state(my_list: list, state: str = "EXECUTED") -> list:
     """Функцция возвращает список словарей,
     если значение state в словаре = "EXECUTED"
@@ -17,3 +20,20 @@ def sort_by_date(my_list: list, is_reverse: bool = True) -> list:
     Возвращает новый список словарей сортированый по дате"""
     list_date_sorted = sorted(my_list, key=lambda date: date["date"], reverse=is_reverse)
     return list_date_sorted
+
+
+def process_bank_search(data:list[dict], search:str)->list[dict]:
+    ''' Принимает список словарей с данными о банковских операциях - data
+     и строку поиска - search, возвращает список словарей, у которых в описании есть search. '''
+    new_list = []
+
+    try:
+        for my_dict in data:
+            descriptions = my_dict["description"]
+            match = re.search(search, descriptions)
+            if match:
+                new_list.append(my_dict)
+    except TypeError:
+        print("Произошла ошибка, проверьте входные данные... Мы получили не то, что ожидали.")
+
+    return new_list
