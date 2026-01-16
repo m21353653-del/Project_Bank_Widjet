55bf46c39033bb734c935058fffc407754836361
m21353653-del <m21353653@gmail.com>
2026-01-16 13:04:51 +0900

    add func process_bank_operations in src/processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,5 @@
 import re
+from collections import Counter
 
 
 def filter_by_state(my_list: list, state: str = "EXECUTED") -> list:
@@ -26,14 +27,35 @@ def process_bank_search(data:list[dict], search:str)->list[dict]:
     ''' Принимает список словарей с данными о банковских операциях - data
      и строку поиска - search, возвращает список словарей, у которых в описании есть search. '''
     new_list = []
+    pattern = re.compile(search)
 
     try:
         for my_dict in data:
             descriptions = my_dict["description"]
-            match = re.search(search, descriptions)
+            match = re.search(pattern, descriptions)
             if match:
                 new_list.append(my_dict)
     except TypeError:
         print("Произошла ошибка, проверьте входные данные... Мы получили не то, что ожидали.")
 
     return new_list
+
+
+def process_bank_operations(data:list[dict], categories:list)-> dict:
+    ''' Получаем список операций и список категорий, возвращаем словарь - 'категория:кол-во операций' '''
+    list_descriptions = []
+
+    try:
+        for my_dict in data:
+            descriptions = my_dict["description"]
+            if descriptions in categories:
+                list_descriptions.append(descriptions)
+
+        dict_result = Counter(list_descriptions)
+        for el in categories:
+            if el not in dict_result:
+                dict_result[el] = 0
+    except TypeError:
+        print("Произошла ошибка, проверьте входные данные... Мы получили не то, что ожидали.")
+
+    return dict_result
